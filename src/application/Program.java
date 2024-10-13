7328acf3fbaaf1f2aa33b7fdae46b2561c02eb78
Ziery-Dev <zieryDev@gmail.com>
2024-10-12 21:31:03 -0300

    Classe dapartamento criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import models.entities.Department;
+
 public class Program {
 	
 	public static void main(String[] args) {
-
 		
+		Department dp = new Department(1, "Books");
+		
+		System.out.println(dp);
 
 	}
 }
---
+++ src/models/entities/Department.java
@@ -0,0 +1,69 @@
+package models.entities;
+
+import java.util.Objects;
+
+public class Department {
+	private Integer id;
+	private String name;
+
+	
+	
+	//construtor
+	public Department() {
+	}
+	
+	public Department(
+			Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	
+	}
+
+
+	//getters e setters
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	//HashCode e equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+
+	//Método toString
+	@Override
+	public String toString() {
+		return "Department [name=" + name + ", id=" + id + "]";
+	}
+	
+	
+	
+	
+}
